db7c366d274c9a7eb5502ff81a672f76455d8106
Cyvior <bryanceazartabanas@gmail.com>
2026-04-27 00:53:09 +0800

    New v5

--- single-product.php
+++ single-product.php
@@ -2,6 +2,7 @@
 
 <main class="zc-single-product-page">
   <?php get_template_part('woocommerce/section/gallery-product'); ?>
+  <?php get_template_part('template-parts/sections/home-how-it-works'); ?>
 </main>
 
 <?php get_footer(); ?>
