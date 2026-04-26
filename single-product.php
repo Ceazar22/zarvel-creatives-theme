0665ef5746b6e8f56d0bdb556487b5fc5099913c
Cyvior <bryanceazartabanas@gmail.com>
2026-04-27 00:59:24 +0800

    Testimonial on product

--- single-product.php
+++ single-product.php
@@ -3,6 +3,7 @@
 <main class="zc-single-product-page">
   <?php get_template_part('woocommerce/section/gallery-product'); ?>
   <?php get_template_part('template-parts/sections/home-how-it-works'); ?>
+  <?php get_template_part('template-parts/sections/product-info-reviews'); ?>
 </main>
 
 <?php get_footer(); ?>
