8974bd5a3bb1c7ab33a2ddce8ca25517e7b90b13
Cyvior <bryanceazartabanas@gmail.com>
2026-04-27 18:08:16 +0800

    Fixed

--- functions.php
+++ functions.php
@@ -1,10 +1,47 @@
 <?php
+defined('ABSPATH') || exit;
+
+/**
+ * Theme setup
+ */
+function zarvel_creative_setup() {
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('custom-logo');
+    add_theme_support('woocommerce');
+
+    register_nav_menus([
+        'primary' => 'Primary Menu',
+        'footer'  => 'Footer Menu',
+    ]);
+}
+add_action('after_setup_theme', 'zarvel_creative_setup');
+
+
+/**
+ * Load main stylesheet
+ */
+function zarvel_creative_scripts() {
+    wp_enqueue_style(
+        'zarvel-style',
+        get_stylesheet_uri(),
+        [],
+        wp_get_theme()->get('Version')
+    );
+}
+add_action('wp_enqueue_scripts', 'zarvel_creative_scripts');
+
+
+/**
+ * Custom template router
+ * This allows page templates to live inside /pages/
+ */
 function zarvel_custom_template_router($template) {
     if (is_admin()) {
         return $template;
     }
 
-    $front_page_template = get_template_directory() . '/pages/front-page.php';
+    $front_page_template     = get_template_directory() . '/pages/front-page.php';
     $single_product_template = get_template_directory() . '/pages/single-product.php';
 
     if (is_front_page() && file_exists($front_page_template)) {
@@ -12,9 +49,7 @@ function zarvel_custom_template_router($template) {
     }
 
     if (
-        (
-            function_exists('is_product') && is_product()
-        ) ||
+        (function_exists('is_product') && is_product()) ||
         is_singular('product')
     ) {
         if (file_exists($single_product_template)) {
@@ -25,4 +60,3 @@ function zarvel_custom_template_router($template) {
     return $template;
 }
 add_filter('template_include', 'zarvel_custom_template_router', 99);
-?>
--- pages/front-page.php
+++ pages/front-page.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 
 <main class="zc-homepage">
-  <?php get_template_part('template-parts/sections/home-hero'); ?>
-  <?php get_template_part('template-parts/sections/shop-categories'); ?>
-  <?php get_template_part('template-parts/sections/home-how-it-works'); ?>
-  <?php get_template_part('template-parts/sections/best-selling'); ?>
-  <?php get_template_part('template-parts/sections/custom-design'); ?>
-  <?php get_template_part('template-parts/sections/why-love'); ?>
-  <?php get_template_part('template-parts/sections/testimonials'); ?>
+  <?php get_template_part('template-parts/home-hero'); ?>
+  <?php get_template_part('template-parts/shop-categories'); ?>
+  <?php get_template_part('template-parts/home-how-it-works'); ?>
+  <?php get_template_part('template-parts/best-selling'); ?>
+  <?php get_template_part('template-parts/custom-design'); ?>
+  <?php get_template_part('template-parts/why-love'); ?>
+  <?php get_template_part('template-parts/testimonials'); ?>
 </main>
 
 <?php get_footer(); ?>
