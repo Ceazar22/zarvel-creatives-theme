c2c16bfa9157546b2dcea202e5d89aafd2630c9a
Cyvior <bryanceazartabanas@gmail.com>
2026-04-28 01:45:20 +0800

    Test

--- pages/customize.php
+++ pages/customize.php
@@ -7,6 +7,7 @@ get_header();
 <main class="zc-customize-page">
   <?php get_template_part('template-parts/customize-hero'); ?>
   <?php get_template_part('template-parts/customize-form'); ?>
+  <?php get_template_part('template-parts/home-how-it-works'); ?>
 </main>
 
 <?php
