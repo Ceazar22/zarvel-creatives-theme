59dfe982507fc4ded668d939f554b9c4cb749c9a
Cyvior <bryanceazartabanas@gmail.com>
2026-04-27 18:11:25 +0800

    Test

--- pages/single-product.php
+++ pages/single-product.php
@@ -1,10 +1,20 @@
-<?php get_header(); ?>
+<?php
+defined('ABSPATH') || exit;
+
+get_header();
+?>
 
 <main class="zc-single-product-page">
-  <?php get_template_part('woocommerce/section/gallery-product'); ?>
+
+  <?php get_template_part('template-parts/woocommerce/sections/gallery-product'); ?>
+
   <?php get_template_part('template-parts/sections/home-how-it-works'); ?>
-  <?php get_template_part('woocommerce/section/product-info-reviews'); ?>
-  <?php get_template_part('woocommerce/section/product-you-may-like'); ?>
+
+  <?php get_template_part('template-parts/woocommerce/sections/product-info-reviews'); ?>
+
+  <?php get_template_part('template-parts/woocommerce/sections/product-you-may-like'); ?>
+
 </main>
 
-<?php get_footer(); ?>
+<?php
+get_footer();
